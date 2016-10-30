2eb713148fc78fa0bcce15859644f2d15c632d4a
verashenyang <shenyang.bupt@yahoo.com>
2016-10-30 16:12:32 -0400

    Update

--- app/src/main/java/com/example/verasy/officehours/MainActivity.java
+++ app/src/main/java/com/example/verasy/officehours/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.verasy.officehours;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import static com.example.verasy.officehours.R.id.courses;
+import static com.example.verasy.officehours.R.id.prof;
 
 //import android.support.v7.app.AppCompatActivity;
 
@@ -17,19 +26,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FrameLayout prof = (FrameLayout)findViewById(R.id.prof);
+        prof.setVisibility(View.VISIBLE);
+        FrameLayout courses = (FrameLayout)findViewById(R.id.courses);
+        courses.setVisibility(View.INVISIBLE);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.courses_list, courselist);
 
         ListView listView = (ListView) findViewById(R.id.listview);
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.courses_list, courselist);
         listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if(courselist[position]=="CS591"){
-                    Intent i = new Intent(MainActivity.this,CourseActivity.class);
-                    startActivity(i);
-                }
-            }
-        });
     }
 }
