2f9a1cf4e5a5913413c3736e931b62286999ed87
Aleksander Skjoelsvik <alekplay@hotmail.com>
2016-11-06 16:39:33 -0500

    Added classes to search activity

--- app/src/main/java/com/example/verasy/officehours/SearchActivity.java
+++ app/src/main/java/com/example/verasy/officehours/SearchActivity.java
@@ -8,18 +8,28 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class SearchActivity extends AppCompatActivity {
 
     //TODO: this needs to be removed when we populate data via SQL searches
     String [] dummyData = {"Homer", "Shereif", "Fake Class", "Blah Blah", "other stuff", "Nonsense", "cuckoo"};
+    HashMap<String, Object> classes = new HashMap<String, Object>();
+    ArrayList<String> classesNames = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.search_result, dummyData);
+        final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.search_result, classesNames);
 
         ListView listView = (ListView) findViewById(R.id.searchResults);
         listView.setAdapter(adapter);
@@ -33,5 +43,33 @@ public class SearchActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Get reference to database
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        // Get reference to classes dictionary
+        DatabaseReference coursesReference = databaseReference.child("classes");
+
+        // Add single value event listener for classes
+        ValueEventListener coursesListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                // Store classes in classes dictionary
+                classes  = (HashMap<String, Object>) dataSnapshot.getValue();
+
+                // Get all keys and add to classesName array
+                classesNames.addAll(classes.keySet());
+
+                // Update listview
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        coursesReference.addListenerForSingleValueEvent(coursesListener);
     }
 }
