786f7acc84a3ab0e5b3dccaa33defe665d4e29ec
joe driver <joewdriver@gmail.com>
2016-11-05 22:49:34 -0400

    added integration with the main activities

--- app/src/main/java/com/example/verasy/officehours/SearchFragment.java
+++ app/src/main/java/com/example/verasy/officehours/SearchFragment.java
@@ -1,10 +1,12 @@
 package com.example.verasy.officehours;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by verasy on 10/25/16.
@@ -14,7 +16,15 @@ public class SearchFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.search_fragment, container, false);
-        return view;
 
+        Button btnSearch = (Button)view.findViewById(R.id.search);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
