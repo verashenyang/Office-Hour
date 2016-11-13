0904570bd950f4e7f7ed484002991ad1d0beb659
verashenyang <shenyang.bupt@yahoo.com>
2016-11-13 12:08:01 -0500

    put text to strings.xml

--- app/src/main/java/com/example/verasy/officehours/MainActivity.java
+++ app/src/main/java/com/example/verasy/officehours/MainActivity.java
@@ -3,22 +3,22 @@ package com.example.verasy.officehours;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ListView;
-import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import io.fabric.sdk.android.Fabric;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "h9sQY3kRKfXZHMkk4jjwKjAsR";
     private static final String TWITTER_SECRET = "gTNRcSzmg1jDngvhvEpn6g7EbMwLyW8ZNejWOcC1xcmY0909PF";
-
+    private Intent i;//get intent from SearchActivity to see which fragment we should show
+    private String type;
     String[] courselist = {"CS591","CS112"};
 
     @Override
@@ -35,11 +36,23 @@ public class MainActivity extends AppCompatActivity {
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
-        FrameLayout prof = (FrameLayout)findViewById(R.id.prof);
+        FrameLayout prof = (FrameLayout) findViewById(R.id.prof);
         prof.setVisibility(View.VISIBLE);
-        FrameLayout courses = (FrameLayout)findViewById(R.id.courses);
+        FrameLayout courses = (FrameLayout) findViewById(R.id.courses);
         courses.setVisibility(View.INVISIBLE);
-
+/*        Intent i = getIntent();
+        type = i.getStringExtra("type");
+        if(type == "prof") {
+            FrameLayout prof = (FrameLayout) findViewById(R.id.prof);
+            prof.setVisibility(View.VISIBLE);
+            FrameLayout courses = (FrameLayout) findViewById(R.id.courses);
+            courses.setVisibility(View.INVISIBLE);
+        }else if (type == "classes") {
+            FrameLayout prof = (FrameLayout) findViewById(R.id.prof);
+            prof.setVisibility(View.INVISIBLE);
+            FrameLayout courses = (FrameLayout) findViewById(R.id.courses);
+            courses.setVisibility(View.VISIBLE);
+        }*/
         //initial proffragment
         initialProf();
         // Get reference to the Firebase Real Time Database
@@ -62,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         database.addValueEventListener(testListener);
+
+
     }
 
 
