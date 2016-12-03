a6aeb7d3aa4bec3ab19d2716ae46c2e7dadb1a50
verashenyang <shenyang.bupt@yahoo.com>
2016-12-03 15:47:58 -0500

    connected to course activity

--- app/src/main/java/com/example/verasy/officehours/RankingActivity.java
+++ app/src/main/java/com/example/verasy/officehours/RankingActivity.java
@@ -1,9 +1,12 @@
 package com.example.verasy.officehours;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -12,7 +15,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -20,42 +22,42 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.StringTokenizer;
 
-import static android.R.attr.rating;
-import static com.example.verasy.officehours.R.id.activity_main;
-import static com.example.verasy.officehours.R.id.listview;
+import android.os.Handler;
+
 
 /**
  * Created by verasy on 11/23/16.
  */
 
-public class RankingActivity extends Activity{
-    private DatabaseReference mDatabase,profRef;
+public class RankingActivity extends Activity {
     ListView listview;
     TextView tv1;
-    HashMap<String, String> databaseEntries = new HashMap<>();
+    HashMap<String, Object> databaseEntries = new HashMap<>();
+    private DatabaseReference mDatabase, profRef;
+    private Handler mHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
 
-        tv1 = (TextView)findViewById(R.id.tv1);
-        listview = (ListView)findViewById(R.id.ranking_list);
+        tv1 = (TextView) findViewById(R.id.tv1);
+        listview = (ListView) findViewById(R.id.ranking_list);
 
         //Get reference to db
         mDatabase = FirebaseDatabase.getInstance().getReference();
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
 
         //Get reference to reviews
         profRef = mDatabase.child("reviews");
 
         //Store professor and the corresponding rating array index in prof_index
-        final Map<String,ArrayList<String>> prof_list = new HashMap<>();
+        final Map<String, ArrayList<String>> prof_list = new HashMap<>();
 
         //Store professor ranking
         final ArrayList<professor> list = new ArrayList<>();
@@ -63,58 +65,92 @@ public class RankingActivity extends Activity{
 
         profRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onDataChange(final DataSnapshot dataSnapshot) {
                 tv1.setText("The 10 best rated professors:");
-                for(DataSnapshot snapshot:dataSnapshot.getChildren()){
-                    GenericTypeIndicator<Map<String,String>> genericTypeIndicator = new GenericTypeIndicator<Map<String, String>>() {
-                    };
-                    Map<String, String> map = snapshot.getValue(genericTypeIndicator);
 
-                    //Get the professor and rating
-                    String prof = map.get("professor");
-                    String rating = map.get("rating");
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
 
-                    //If the prof_list contains the professor add the rating to the professor's rating list
-                    if(prof_list.containsKey(prof)){
-                        prof_list.get(prof).add(rating);
-                    //If the prof_list doesn't contain the professor add him/her to the prof_list and then add his/her rating to the rating list
-                    }else{
-                        prof_list.put(prof,new ArrayList<String>());
+                    String prof = snapshot.getKey();
+
+                    //put prof to database
+                    prof_list.put(prof, new ArrayList<String>());
+
+                    for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+
+                        //put rating to professor's rating list
+                        String rating = snapshot1.child("rating").getValue().toString();
                         prof_list.get(prof).add(rating);
                     }
-                    Log.e("Prof","The professor is " + prof+" and the rating is "+ rating);
                 }
 
                 //Calculate the average rating of a professor
                 for (Map.Entry<String, ArrayList<String>> entry : prof_list.entrySet()) {
                     float sum = 0;
                     float average = 0;
-                    for(String enrty:entry.getValue()){
+                    for (String enrty : entry.getValue()) {
                         sum += Float.valueOf(enrty);
                     }
-                    average = sum/entry.getValue().size();
-                    list.add(new professor(entry.getKey(),average));
-                    //Log.e("Average",""+average);
+                    average = sum / entry.getValue().size();
+                    list.add(new professor(entry.getKey(), average));
                 }
 
                 //Sort the professors' ratings
                 Collections.sort(list, new Comparator<professor>() {
                     @Override
                     public int compare(professor o1, professor o2) {
-                        if((o1.average_rating - o2.average_rating) > 0)
-                            return 1;
-                        else return -1;
+                        if ((o1.average_rating - o2.average_rating) > 0)
+                            return -1;
+                        else return 1;
                     }
                 });
 
                 //Get the professors' ranking
                 ArrayList<String> ranking = new ArrayList<String>();
-                for(professor prof : list) {
+                for (professor prof : list) {
                     ranking.add(prof.getProfName());
                 }
 
-                ArrayAdapter adapter = new ArrayAdapter(RankingActivity.this, android.R.layout.simple_list_item_1,ranking.subList(0,10));
+                final ArrayAdapter adapter = new ArrayAdapter(RankingActivity.this, android.R.layout.simple_list_item_1, ranking.subList(0,10));
                 listview.setAdapter(adapter);
+
+                listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        //
+                        final String profClick = (String) listview.getItemAtPosition(position);
+                        Log.e("selection", profClick);
+                        DatabaseReference professor = mDatabase.child("professors").child(profClick);
+
+                        // Add single value event listener for professor type
+                        ValueEventListener professorsListener = new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                // Store professors in professors dictionary
+                                databaseEntries.put(profClick, dataSnapshot.getValue());
+                                Log.e("LOG1", databaseEntries.get(profClick) + "hello");
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+                                Log.e("ExceptionTag", "Cannot get professors from firebase");
+                            }
+                        };
+                        professor.addListenerForSingleValueEvent(professorsListener);
+
+                        //will launch the activity
+                        Runnable mLaunchTask = new Runnable() {
+                            public void run() {
+                                Intent professorIntent = new Intent(RankingActivity.this, ProfessorActivity.class);
+                                professorIntent.putExtra("name", profClick);
+                                professorIntent.putExtra("content", (HashMap<String, String>) databaseEntries.get(profClick));
+                                startActivity(professorIntent);
+                            }
+                        };
+
+                        //cause some delay to wait for result from database
+                        mHandler.postDelayed(mLaunchTask, 50);
+                    }
+                });
             }
 
             @Override
@@ -125,17 +161,19 @@ public class RankingActivity extends Activity{
     }
 
     //Object professor
-    class professor{
+    class professor {
         private float average_rating;
         private String prof;
 
-        public professor(String prof,Float ave){
+        public professor(String prof, Float ave) {
             this.prof = prof;
             this.average_rating = ave;
         }
-        public String getProfName(){
+
+        public String getProfName() {
             return prof;
         }
+
         public float getProfRating() {
             return average_rating;
         }
