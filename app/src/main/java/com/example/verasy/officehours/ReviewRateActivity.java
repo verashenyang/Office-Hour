5ae13a2ef0d057ff2fab59014d4d5c68c0812dc2
Jian Zhou <jianzhou@dhcp-wifi-8021x-155-41-104-152.bu.edu>
2016-12-03 15:18:08 -0500

    all done this part

--- app/src/main/java/com/example/verasy/officehours/ReviewRateActivity.java
+++ app/src/main/java/com/example/verasy/officehours/ReviewRateActivity.java
@@ -64,31 +64,12 @@ public class ReviewRateActivity extends AppCompatActivity {
         average_score = (TextView)findViewById(R.id.average_score);
         btn_voice = (Button)findViewById(R.id.btn_voice);
 
+        //Get intent from ProfessorActivity
         Intent intent = getIntent();
         Bundle intentBundle = intent.getExtras();
 
+        //Get the profName by getExtras from intent
         final String profName = (String)intentBundle.get("ProfName");
-
-        btn_voice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-us");
-                i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Talk to Me!");  //this is what the user sees before they talk.
-
-                try
-                {
-                    startActivityForResult(i, 999);  //the 999 serves as our request code (breadcrumb), so we know what to phish for if we start other activities for results.
-                }
-                catch (ActivityNotFoundException e)
-                {
-                    Toast.makeText(ReviewRateActivity.this, "Microphone not available.  Does you device have a Mic?", Toast.LENGTH_LONG);
-                }
-
-            }
-        });
-
 
         // using an ArrayList to hold all matched tuple
         final ArrayList<ReviewObject> objects = new ArrayList<ReviewObject>();
@@ -96,45 +77,60 @@ public class ReviewRateActivity extends AppCompatActivity {
         final CustomAdapter customAdapter = new CustomAdapter(this, objects);
         reviewList.setAdapter(customAdapter);
 
-        //Get reference to the FireBase about dictionary "reviews"
+        //Forward reference to dictionary "reviews" in FireBase
         DatabaseReference reviewsRef = mDatabase.child("reviews");
 
+        //Add value event listener
+        //[start lister]
         ValueEventListener reviewListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+
+                //Get reviews object and use the values to update the UI
                 databaseEntries = (HashMap<String,Object>) dataSnapshot.getValue();
 
+                //Create an arrayList to hold every comment and rate about the professor
+                //comment and rate is the value of the HashMap, and the index is the key of the HashMap
                 ArrayList<HashMap<String,String>> content = new ArrayList<>();
 
+                //To hold the total rating of the professor
                 float total = 0f;
 
+                //Refresh the objects arrayList once this method is called again
                 objects.clear();
 
+                //Get the data of the professor searched
                 for(String key: databaseEntries.keySet()){
                     if(key.equals(profName)){
                         content = (ArrayList<HashMap<String,String>>)databaseEntries.get(key);
                         break;
+                        //Once find it, jump out.
                     }
                 }
 
-
+                //adding every comment and rating into tuple object
                 for(HashMap<String,String> item: content){
                     if(item.get("comment")!=null && item.get("rating")!=null){
                         float rate = Float.valueOf(String.valueOf(item.get("rating")));
                         ReviewObject tuple = new ReviewObject(profName,item.get("comment"),rate);
                         total += tuple.rating;
+                        //append every tuple to the objects arrayList
                         objects.add(tuple);
                     }
                 }
-
+                //Get the total size of comments for the professor
                 size = objects.size();
-                Log.e("Boston", Integer.toString(objects.size()));
 
+                //get the average rating of the professor
                 float rate = total/content.size();
+                //only keep one decimal of the float number
                 DecimalFormat df = new DecimalFormat("#.0");
+                //set the rating bar to the average rating
                 average_rating.setRating(rate);
+                //set the TextView showing the average rating
                 average_score.setText(df.format(rate)+"/5");
 
+                //once a new comment is added, this method will automatically renew the data we need
                 customAdapter.notifyDataSetChanged();
 
             }
@@ -146,6 +142,7 @@ public class ReviewRateActivity extends AppCompatActivity {
         };
 
         reviewsRef.addValueEventListener(reviewListener);
+        //[end lister]
 
 
         // Get the professor rating from RatingBar
@@ -166,6 +163,30 @@ public class ReviewRateActivity extends AppCompatActivity {
                 profRatBar.setRating(0f);
             }
         });
+
+        // Clicking the voiceInput button, users could use microphone to write the comment
+        btn_voice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-us");
+                //Below is what the user sees before they talk.
+                i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Talk to Me!");
+
+                try
+                {
+                    startActivityForResult(i, 999);
+                    //the 999 serves as our request code (breadcrumb), so we know what to phish for if we start other activities for results
+                }
+                catch (ActivityNotFoundException e)
+                {
+                    //populate a toast when the microphone is not available
+                    Toast.makeText(ReviewRateActivity.this, "Microphone not available.  Does you device have a Mic?", Toast.LENGTH_LONG);
+                }
+
+            }
+        });
     }
 
     // method for writing comment and rating of the professor into FireBase
@@ -174,6 +195,7 @@ public class ReviewRateActivity extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference();
         String newIndex = Integer.toString(size);
         Log.e("hello",newIndex);
+        //put the data into correct location of the FireBase
         mDatabase.child("reviews").child(prof_name).child(newIndex).child("rating").setValue(rate);
         mDatabase.child("reviews").child(prof_name).child(newIndex).child("comment").setValue(comment);
     }
